e67e0707938c3957175d4864bbe04db54a79c6ba
tsubo2025 <201512326+tsubo2025@users.noreply.github.com>
2025-06-26 15:38:43 +0900

    設定ページの配色変更を、できるようにしました。

--- tomato_diary/resources/views/layouts/app.blade.php
+++ tomato_diary/resources/views/layouts/app.blade.php
@@ -14,7 +14,7 @@
     <link href="{{ asset('css/custom.css') }}" rel="stylesheet">
     @vite('resources/css/app.css') {{-- <-- mouse_stalker --}}
 </head>
-<body>
+<body class="{{ \App\Helpers\ThemeHelper::getThemeClass() }}">
     <div class="d-flex flex-column min-vh-100">
         <x-header />
 
